72d0c81905180313ca58c107bcb1cdd40f2bf679
Joungsik <tjstlr2010@gmail.com>
2021-08-11 23:53:12 +0900

    [Add] about container

---
+++ components/organisms/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = ({}) => {
+  return (
+    <header className={"navbar navbar-expand-lg navbar-light fixed-top py-3"}>
+      <div className={"container"}>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
