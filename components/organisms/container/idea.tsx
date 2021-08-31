6ce2741733b5ddf69f3ac0f0787c111c41bd2c5c
JoungSik <tjstlr2010@gmail.com>
2021-08-31 16:15:14 +0900

    [Change] Item 추가 안되던 이슈 수정

--- components/organisms/container/idea.tsx
+++ components/organisms/container/idea.tsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import faker from 'faker'
 
 import styles from '../../../styles/container/idea.module.css'
 import IdeaItem from '../../molecules/item/idea'
 
-const IdeaContainer = ({}) => {
-  const [items, setItems] = useState(Array(12).map((_, index) => ({
-    id: index,
-    title: `이름 - ${index}`,
-    subTitle: '소제목',
-    content: '내용',
-    image: faker.image.imageUrl(),
-  })));
+interface item {
+  id: number;
+  title: string;
+  subTitle: string;
+  content: string;
+  image: string;
+}
+
+const IdeaContainer = () => {
+  const [items, setItems] = useState(Array<item>(0));
+
+  useEffect(() => {
+    const temp = new Array<item>(0);
+    for (let i = 0; i < 12; i++) {
+      temp.push({
+        id: i,
+        title: `이름 - ${i}`,
+        subTitle: '소제목',
+        content: '내용',
+        image: faker.image.imageUrl(),
+      });
+    }
+    setItems(temp);
+  }, []);
 
   return (
     <section className={styles.section}>
